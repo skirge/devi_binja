a5bbb1d1c5cbde53c6b71baf30bfba7b80350bf4
skirge <skirge84@o2.pl>
2024-01-07 15:19:56 +0100

    updates and fixes for better work with big binaries (smaller undo history)

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from binaryninja import interaction
 from binaryninja import InstructionTextTokenType
 from binaryninja import demangle
 from binaryninja import log
+from binaryninja.variable import *
 import json
 import traceback
 import os
@@ -18,43 +19,57 @@ class binja_devi():
 
     def load_virtual_calls(self):
         json_file = interaction.get_open_filename_input("Load virtual calls", "*.json")
-        with open(json_file.decode("utf-8")) as f:
+        with open(json_file) as f:
             devi_json_data = json.load(f)
-                
         if self.version < devi_json_data["deviVersion"]:
             print("[!] devi JSON file has a more recent version than IDA plugin!")
             print("[!] we try parsing anyway!")
         if self.version > devi_json_data["deviVersion"]:
             print("[!] Your devi_ida and devi_frida versions are out of sync. Update your devi_ida!")
 
+        state = self.bv.begin_undo_actions()
         if self.version == devi_json_data["deviVersion"]:
             self.devirtualize_calls(devi_json_data["calls"], devi_json_data["modules"])
         elif devi_json_data["deviVersion"] == 0.1:
             self.devirtualize_calls_v01(devi_json_data["calls"])
+        self.bv.commit_undo_actions(state)
 
 
     def devirtualize_calls(self, call_list, modules):
-        binja_filename = os.path.basename(self.bv.file.filename)
+        binja_filename = os.path.basename(self.bv.file.original_filename).replace('.bndb','')
 
         for module in modules:
             if module["name"] == binja_filename:
                 loaded_module = module
                 break
-        
+        if loaded_module is None:
+            print("[-] Module not found!")
+            return 
         start = int(loaded_module["base"], 16)
         end = start + loaded_module["size"]
 
-        print("[!] Adding virtual calls for {binja_filename}")
+        print(f"[!] Adding virtual calls for {binja_filename}")
 
         for v_call in call_list:
             for call in v_call:
                 if start <= int(call, 16) <= end:
+                    print(call)
 
                     src = int(call, 16) - start
                     dst = int(v_call[call]) - start
 
-                    self.caller = self.bv.get_functions_containing(src)[0]
-                    self.caller.add_user_code_ref(src, dst, from_arch=None)
+                    src += self.bv.start
+                    dst += self.bv.start
+
+                    # print(hex(src))
+                    # print(hex(dst))
+                    funcs = self.bv.get_functions_containing(src)
+                    if len(funcs) == 0:
+                        log.log(1, "[-] Functions for address {} not found".format(src))
+                        continue
+
+                    self.caller = funcs[0]
+                    self.caller.add_user_code_ref(src, dst)
                     self.add_call_comment(src, dst)
                     self.call_cnt += 1
         log.log(1, "Devirtualized {} calls".format(self.call_cnt))
@@ -66,18 +81,22 @@ class binja_devi():
                 to_addr = int(v_call[call])
                 #print(hex(int(call)))
                 #print(hex(to_addr))
+                #print(hex(int(call)))
+                #print(hex(to_addr))
                 from_addr = int(call)
                 self.caller = self.bv.get_functions_containing(from_addr)[0]
                 self.caller.add_user_code_ref(from_addr, to_addr, from_arch=None)
                 self.add_call_comment(from_addr, to_addr)
                 self.call_cnt += 1
         log.log(1, "Devirtualized {} calls".format(self.call_cnt))
-                
 
     def add_call_comment(self, from_addr, to_addr):
         to_func = self.bv.get_function_at(to_addr)
+        if to_func is None or to_func.name is None:
+            return
         _, name = demangle.demangle_gnu3(self.bv.arch, to_func.name)
         old_comment = self.caller.get_comment_at(from_addr)
-        self.caller.set_comment_at(from_addr, demangle.get_qualified_name(name) + "\n" + old_comment)
+        if name not in old_comment:
+            self.caller.set_comment_at(from_addr, name + "\n" + old_comment)
 
 PluginCommand.register("devi", "DEvirtualize VIrtual calls", binja_devi)
